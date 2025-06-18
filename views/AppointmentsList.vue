ab2240fb8e24958c94ecfcccb81eb9342761ea15
Alaa  Jamal Abdalqader <amalaa20@cit.just.edu.jo>
2025-06-18 03:31:40 +0300

    add APIs

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -59,7 +59,7 @@ export default {
   },
   methods: {
     fetchAppointments() {
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments")
+      fetch("https://u3zgld.execute-api:us-east-1:730335665786:hldnlmhee0/*/GET/appointments")
         .then(res => res.json())
         .then(data => {
           const parsed = JSON.parse(data.body);
@@ -74,7 +74,7 @@ export default {
       console.log("appointmentId:", cleanAppointment.appointmentId);
       console.log(" appointmentId (direct):", appointment.appointmentId);
 
-      const url = `https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
+      const url = `https://4pqx5j.execute-api:us-east-1:730335665786:hldnlmhee0/*/PATCH/appointments/{appointmentId}`;
 
       const payload = { status: newStatus };
 
